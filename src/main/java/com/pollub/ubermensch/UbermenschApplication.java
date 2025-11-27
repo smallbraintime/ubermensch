d9b8593762c6bc2912f27bd30c3e24c2259f66b2
Patryk Strużek <patrykstruzek@proton.me>
2025-11-27 22:29:20 +0100

    feat: init spring boot project

---
+++ src/main/java/com/pollub/ubermensch/UbermenschApplication.java
@@ -0,0 +1,13 @@
+package com.pollub.ubermensch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbermenschApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbermenschApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pollub/ubermensch/UbermenschApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pollub.ubermensch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbermenschApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
